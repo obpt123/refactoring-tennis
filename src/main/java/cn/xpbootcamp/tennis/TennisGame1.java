e24f13569014b90cf3161f84184bfa043a898ec4
ypb <obpt@sina.com>
2020-03-03 12:27:43 +0800

    TennisGame1 [重复代码] player1 和player2 重复且被写死

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -13,7 +13,7 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if (playerName == player1Name)
             m_score1 += 1;
         else
             m_score2 += 1;
@@ -21,57 +21,82 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
+        if (m_score1 == m_score2) {
+            score = getEqualsScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            score = getHigherScore();
+        } else {
+            score = getLowerScore(score);
+        }
+        return score;
+    }
 
+    private String getLowerScore(String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
             }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+            switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
             }
         }
         return score;
+    }
+
+    private String getHigherScore() {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1)
+            score = formatPlayer1("Advantage %s");
+        else if (minusResult == -1)
+            score = formatPlayer2("Advantage %s");
+        else if (minusResult >= 2)
+            score = formatPlayer1("Win for %s");
+        else
+            score = formatPlayer2("Win for %s");
+        return score;
+    }
+
+    private String getEqualsScore() {
+        String score;
+        switch (m_score1) {
+        case 0:
+            score = "Love-All";
+            break;
+        case 1:
+            score = "Fifteen-All";
+            break;
+        case 2:
+            score = "Thirty-All";
+            break;
+        default:
+            score = "Deuce";
+            break;
+
+        }
+        return score;
+    }
+
+    private String formatPlayer1(String fmt) {
+        return String.format(fmt, this.player1Name);
+    }
+
+    private String formatPlayer2(String fmt) {
+        return String.format(fmt, this.player2Name);
     }
 }
