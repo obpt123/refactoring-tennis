efa79b33f3c1d2ef34f7278d6df9b3e28890b5ad
ypb <obpt@sina.com>
2020-03-03 20:17:25 +0800

    TennisGame3 [神秘命名] 更改为有意义的命名

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -2,36 +2,36 @@ package cn.xpbootcamp.tennis;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    static final String[] LOWER_NAMES = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
 
-    public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+    private int score2;
+    private int score1;
+    private String player1;
+    private String player2;
+
+    public TennisGame3(String player1Name, String player2Name) {
+        this.player1 = player1Name;
+        this.player2 = player2Name;
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+        String temp;
+        if (score1 < 4 && score2 < 4 && !(score1 + score2 == 6)) {
+            temp = LOWER_NAMES[score1];
+            return (score1 == score2) ? temp + "-All" : temp + "-" + LOWER_NAMES[score2];
         } else {
-            if (p1 == p2)
+            if (score1 == score2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            temp = score1 > score2 ? player1 : player2;
+            return ((score1 - score2) * (score1 - score2) == 1) ? "Advantage " + temp : "Win for " + temp;
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.score1 += 1;
         else
-            this.p2 += 1;
-
+            this.score2 += 1;
     }
 
 }
