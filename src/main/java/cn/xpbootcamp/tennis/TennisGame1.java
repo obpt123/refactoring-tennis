72de4462f86ba2a9ab72eb83c2e37a34d1df21b4
ypb <obpt@sina.com>
2020-03-08 14:38:24 +0800

    使用变量名称代替常量

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -13,10 +13,11 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName.equals(player1Name)) {
+            m_score1++;
+        } else {
+            m_score2++;
+        }
     }
 
     public String getScore() {
