e7b03eb8475aaf0199822d0cf17f3412ce6d19a8
ypb <obpt@sina.com>
2020-03-08 14:34:53 +0800

    split long method

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -20,57 +20,72 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
+        if (m_score1 == m_score2) {
+            return getEqualScore();
+        } else if (m_score1 >= 4 || m_score2 >= 4) {
+            return getHigherScore();
+        } else {
+            return getLowerScore("");
+        }
+    }
 
+    private String getLowerScore(String score) {
+        int tempScore;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1)
+                tempScore = m_score1;
+            else {
+                score += "-";
+                tempScore = m_score2;
+            }
+            switch (tempScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
+        return score;
+    }
+
+    private String getHigherScore() {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1)
+            score = "Advantage player1";
+        else if (minusResult == -1)
+            score = "Advantage player2";
+        else if (minusResult >= 2)
+            score = "Win for player1";
         else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            score = "Win for player2";
+        return score;
+    }
+
+    private String getEqualScore() {
+        String score;
+        switch (m_score1) {
+        case 0:
+            score = "Love-All";
+            break;
+        case 1:
+            score = "Fifteen-All";
+            break;
+        case 2:
+            score = "Thirty-All";
+            break;
+        default:
+            score = "Deuce";
+            break;
+
         }
         return score;
     }
