d16de21bbaa887b4537bc9742c535e7e147657aa
ypb <obpt@sina.com>
2020-03-08 14:48:10 +0800

    优化getLowerScore

--- src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame1.java
@@ -31,30 +31,8 @@ public class TennisGame1 implements TennisGame {
     }
 
     private String getLowerScore(String score) {
-        int tempScore;
-        for (int i = 1; i < 3; i++) {
-            if (i == 1)
-                tempScore = m_score1;
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-            case 0:
-                score += "Love";
-                break;
-            case 1:
-                score += "Fifteen";
-                break;
-            case 2:
-                score += "Thirty";
-                break;
-            case 3:
-                score += "Forty";
-                break;
-            }
-        }
-        return score;
+        String[] lowerScoreNames = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
+        return String.format("%s-%s", lowerScoreNames[m_score1], lowerScoreNames[m_score2]);
     }
 
     private String getHigherScore() {
